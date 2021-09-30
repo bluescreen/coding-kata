5d693021a5d28d9881f3006ccfaef2e0928614f2
Markus Muschol <markus.muschol@denkwerk.com>
2021-09-30 08:57:59 +0200

    Prepared

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// Für Vielfache von 3 gibt „Fizz“ aus.
+// Für Vielfache von 5 gib „Buzz“ aus.
+// Für Vielfache von 3 und 5 gib „FizzBuzz“ aus.
+// Beispiel Ausgabe
+// 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
