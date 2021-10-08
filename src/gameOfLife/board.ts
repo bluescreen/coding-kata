affb1241ab310f1e9b35def63c8573cd12925a4e
Markus Muschol <markus.muschol@denkwerk.com>
2021-10-08 08:49:32 +0200

    Board index

--- src/gameOfLife/board.ts
+++ src/gameOfLife/board.ts
@@ -1,80 +1,98 @@
-import Cell from "./cell";
-import { CellState } from "./cellState";
+import Cell from './cell';
+import { CellState } from './cellState';
 
-export default class Board{
-    private cells:Cell[]  = [];
+export default class Board {
+  private cells: Cell[] = [];
 
-    constructor(public rows: number, public columns: number){
-        for(let row = 0; row < rows; row++){
-            for(let col = 0; col < columns; col++){
-                this.cells[this.columns * row + col] = new Cell(CellState.DEAD);
-            }
+  constructor(public rows: number, public columns: number) {
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < columns; col++) {
+        this.cells[this.columns * row + col] = new Cell(CellState.DEAD);
+      }
+    }
+  }
+
+  get numCells() {
+    return this.cells.length;
+  }
+
+  setState(state: string): void {
+    const states: string[] = state.split('');
+    for (let i = 0; i < states.length; i++) {
+      this.cells[i].state =
+        states[i] === '.' ? CellState.DEAD : CellState.ALIVE;
+    }
+  }
+
+  findCoordsFor(index) {
+    const col = Math.trunc(((index - 1) % this.columns) + 1);
+    const row = Math.trunc((index - 1) / this.columns + 1);
+
+    return { row, col };
+  }
+
+  findNeighbours(index: number): Cell[] {
+    const neighbours = [];
+    const coords = this.findCoordsFor(index);
+    const col = coords.col;
+    const row = coords.row;
+
+    var rowLimit = this.rows - 1;
+    var columnLimit = this.columns - 1;
+
+    for (
+      let y = Math.max(0, col - 1);
+      y <= Math.min(col + 1, columnLimit);
+      y++
+    ) {
+      for (
+        let x = Math.max(0, row - 1);
+        x <= Math.min(row + 1, rowLimit);
+        x++
+      ) {
+        if (x !== col || y !== row) {
+          //console.log('Neighbour',x,y,this.columns * x + y);
+          neighbours.push(this.cells[this.columns * x + y]);
         }
+      }
     }
 
-    get numCells(){
-        return this.cells.length
+    return neighbours;
+  }
+
+  findLivingNeighbours(index) {
+    return this.findNeighbours(index).filter(
+      (cell: Cell) => cell.state === CellState.ALIVE,
+    ).length;
+  }
+
+  update() {
+    const updated = [...this.cells];
+    for (let i = 1; i < updated.length; i++) {
+      const isAlive = this.cells[i].state;
+      const numAlive = this.findLivingNeighbours(i);
+      console.log(
+        i,
+        '->',
+        'alive',
+        isAlive,
+        'aliveNighbours',
+        numAlive,
+        this.findCoordsFor(i),
+      );
+      updated[i].tick(numAlive);
     }
+    this.cells = updated;
+  }
 
-    setState(state: string): void {
-        const states:string[] = state.split('');
-        for(let i = 0;i < states.length; i++){ 
-            this.cells[i].state = states[i] === '.' ? CellState.DEAD : CellState.ALIVE
-        }
+  render() {
+    let output: string = '';
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.columns; col++) {
+        output += this.cells[this.columns * row + col].render();
+      }
+      output += '\n';
     }
-
-    findCoordsFor(index){
-
-        const col = Math.trunc((index-1)%this.columns+1);
-        const row = Math.trunc((index-1)/(this.columns)+1);
-
-        return {row,col};
-    }
-
-    findNeighbours(index: number): Cell[]{
-        const neighbours = [];
-        const coords = this.findCoordsFor(index);
-        const col = coords.col;
-        const row = coords.row; 
-
-
-        var rowLimit = this.rows-1;
-        var columnLimit = this.columns-1;
-
-        for(let y = Math.max(0, col-1); y <= Math.min(col+1, columnLimit); y++) {
-            for(let x = Math.max(0, row-1); x <= Math.min(row+1, rowLimit); x++) {
-                if(x !== col|| y !== row) {
-                    //console.log('Neighbour',x,y,this.columns * x + y);
-                    neighbours.push(this.cells[this.columns * x + y]);
-                }
-            }
-        }
-
-        return neighbours;
-    }
-
-    findLivingNeighbours(index){
-        return this.findNeighbours(index).filter((cell: Cell) => cell.state === CellState.ALIVE).length;
-    }
-
-    update(){
-        const updated = [...this.cells];
-        for(let i = 0;i < updated.length; i++){
-            const numAlive = this.findLivingNeighbours(i);
-            console.log(i,'->', numAlive, this.findCoordsFor(i));
-            updated[i].tick(numAlive);
-        }
-        this.cells = updated;
-    }
-
-    render(){
-        let output: string = '';
-        for(let row = 0; row < this.rows; row++){
-            for(let col = 0; col < this.columns; col++){
-                output += this.cells[this.columns *  row + col].render();
-            }
-            output += '\n';
-        }
-        return output;
-    }
+    return output;
+  }
 }
--- tests/gameOfLife.test.ts
+++ tests/gameOfLife.test.ts
@@ -4,190 +4,164 @@
 3. Any live cell with two or three live neighbours lives on to the next generation.
 4. Any dead cell with exactly three live neighbours becomes a live cell.*/
 
-import Board from "../src/gameOfLife/board";
-import Cell from "../src/gameOfLife/cell";
-import { CellState } from "../src/gameOfLife/cellState";
+import Board from '../src/gameOfLife/board';
+import Cell from '../src/gameOfLife/cell';
+import { CellState } from '../src/gameOfLife/cellState';
 
-describe('Convays game of life', ()=>{
-
-    describe('Cell State', ()=> {
-        it('can create and render a living cell', () => {
-            const cell = new Cell(CellState.ALIVE);
-            expect(cell.state).toBe(1);
-            expect(cell.render()).toBe('*');
-        });
-    
-        it('can create and render a dead cell', () => {
-            const cell = new Cell(CellState.DEAD);
-            expect(cell.state).toBe(0);
-            expect(cell.render()).toBe('.');
-
-        });
-    });
-   
-    describe('Cell Behavior', ()=> {
-
-        it('should kill a cell with less than 2 neighbours', () => {
-            const cell = new Cell(CellState.ALIVE);
-            expect(cell.tick(1)).toBe(CellState.DEAD);
-        });
-
-        
-
-        it('should kill a cell with more than 3 neighbours', () => {
-            const cell = new Cell(CellState.ALIVE);
-            expect(cell.tick(4)).toBe(CellState.DEAD);
-        });
-
-
-        it('should stay alive with 2 neighbours', () => {
-            const cell = new Cell(CellState.ALIVE);
-            expect(cell.tick(2)).toBe(CellState.ALIVE);
-        });
-        
-        it('should stay alive with 3 neighbours', () => {
-            const cell = new Cell(CellState.ALIVE);
-            expect(cell.tick(3)).toBe(CellState.ALIVE);
-        });
-
-        it('should revive if cell has 3 alive neighbours', () => {
-            const cell = new Cell(CellState.DEAD);
-            expect(cell.tick(3)).toBe(CellState.ALIVE);
-        });
+describe('Convays game of life', () => {
+  describe('Cell State', () => {
+    it('can create and render a living cell', () => {
+      const cell = new Cell(CellState.ALIVE);
+      expect(cell.state).toBe(1);
+      expect(cell.render()).toBe('*');
     });
 
-    describe('Create Board', () => {
-        it('should create and render a board of dead cells', () => {
-            const board = new Board(4,8);
-            expect(board.rows).toBe(4);
-            expect(board.columns).toBe(8);
-            expect(board.numCells).toEqual(4*8);
-            expect(board.render()).toEqual(
-            '........\n'+
-            '........\n'+
-            '........\n'+
-            '........\n');
-        })
+    it('can create and render a dead cell', () => {
+      const cell = new Cell(CellState.DEAD);
+      expect(cell.state).toBe(0);
+      expect(cell.render()).toBe('.');
+    });
+  });
 
-        it('should parse a preset board', () => {
-            const state = '............*......**...........';
-            const board = new Board(4,8);
-            board.setState(state);
+  describe('Cell Behavior', () => {
+    it('should kill a cell with less than 2 neighbours', () => {
+      const cell = new Cell(CellState.ALIVE);
+      expect(cell.tick(1)).toBe(CellState.DEAD);
+    });
 
-            expect(board.render()).toEqual(
-                '........\n'+
-                '....*...\n'+
-                '...**...\n'+
-                '........\n');
-        })
+    it('should kill a cell with more than 3 neighbours', () => {
+      const cell = new Cell(CellState.ALIVE);
+      expect(cell.tick(4)).toBe(CellState.DEAD);
+    });
 
-        
-    })
+    it('should stay alive with 2 neighbours', () => {
+      const cell = new Cell(CellState.ALIVE);
+      expect(cell.tick(2)).toBe(CellState.ALIVE);
+    });
 
-    describe('Find cell nighbours', () => {
-        it('should find 3 neighbours for first cell', () => {
-            const board = new Board(3,3);
-            const neighbours = board.findNeighbours(1);
-            expect(neighbours.length).toEqual(3);
-        })
+    it('should stay alive with 3 neighbours', () => {
+      const cell = new Cell(CellState.ALIVE);
+      expect(cell.tick(3)).toBe(CellState.ALIVE);
+    });
 
-        it('should find 5 neighbours for second cell', () => {
-            const board = new Board(3,3);
-            const neighbours = board.findNeighbours(2);
-            expect(neighbours.length).toEqual(5);
-        })
+    it('should revive if cell has 3 alive neighbours', () => {
+      const cell = new Cell(CellState.DEAD);
+      expect(cell.tick(3)).toBe(CellState.ALIVE);
+    });
+  });
 
-        it('should find 8 neighbours for center cell', () => {
-            const board = new Board(3,3);
-            const neighbours = board.findNeighbours(5);
-            expect(neighbours.length).toEqual(8);
-        })
+  describe('Create Board', () => {
+    it('should create and render a board of dead cells', () => {
+      const board = new Board(4, 8);
+      expect(board.rows).toBe(4);
+      expect(board.columns).toBe(8);
+      expect(board.numCells).toEqual(4 * 8);
+      expect(board.render()).toEqual(
+        '........\n' + '........\n' + '........\n' + '........\n',
+      );
+    });
 
+    it('should parse a preset board', () => {
+      const state = '............*......**...........';
+      const board = new Board(4, 8);
+      board.setState(state);
 
-        it('should find 3 neighbours for last cell', () => {
-            const board = new Board(3,3);
-            const neighbours = board.findNeighbours(9);
-            expect(neighbours.length).toEqual(3);
-        })
+      expect(board.render()).toEqual(
+        '........\n' + '....*...\n' + '...**...\n' + '........\n',
+      );
+    });
+  });
 
-        it('should find coords for index', () => {
-            const board = new Board(3,3);
-            const coords = board.findCoordsFor(9);
-            expect(coords.col).toBe(3);
-            expect(coords.row).toBe(3);            
-        }) 
+  describe('Find cell nighbours', () => {
+    it('should find 3 neighbours for first cell', () => {
+      const board = new Board(3, 3);
+      const neighbours = board.findNeighbours(1);
+      expect(neighbours.length).toEqual(3);
+    });
 
-        it('should find number of living neighbours from edge', () => {
-            const board = new Board(3,3);
-            board.setState(
-                '.*.'+
-                '**.'+
-                '...');
-            const numAlive = board.findLivingNeighbours(1);
-            expect(numAlive).toEqual(3);
-        })
+    it('should find 5 neighbours for second cell', () => {
+      const board = new Board(3, 3);
+      const neighbours = board.findNeighbours(2);
+      expect(neighbours.length).toEqual(5);
+    });
 
-        it('should find number of living neighbours from center 2', () => {
-            const board = new Board(3,6);
-            board.setState(
-                '...***'+
-                '...*.*'+
-                '...***');
-            const numAlive = board.findLivingNeighbours(11);
-            expect(numAlive).toEqual(8);
-        })
+    it('should find 8 neighbours for center cell', () => {
+      const board = new Board(3, 3);
+      const neighbours = board.findNeighbours(5);
+      expect(neighbours.length).toEqual(8);
+    });
 
-        it('should find number of living neighbours from center', () => {
-            const board = new Board(3,3);
-            board.setState(
-                '***'+
-                '*.*'+
-                '***');
-            const numAlive = board.findLivingNeighbours(5);
-            expect(numAlive).toEqual(8);
-        })
-    })
+    it('should find 3 neighbours for last cell', () => {
+      const board = new Board(3, 3);
+      const neighbours = board.findNeighbours(9);
+      expect(neighbours.length).toEqual(3);
+    });
 
+    it('should find number of living neighbours from edge', () => {
+      const board = new Board(3, 3);
+      board.setState('.*.' + '**.' + '...');
+      const numAlive = board.findLivingNeighbours(1);
+      expect(numAlive).toEqual(3);
+    });
 
-    describe('Run game of life', () => {
+    it('should find number of living neighbours from center 2', () => {
+      const board = new Board(3, 6);
+      board.setState('...***' + '...*.*' + '...***');
+      const numAlive = board.findLivingNeighbours(11);
+      expect(numAlive).toEqual(8);
+    });
 
-        it('should update simple 3x3 board', () => {
-            const board = new Board(3,3);
-            board.setState(
-                '**.'+
-                '*..'+
-                '...');
+    it('should find number of living neighbours from center', () => {
+      const board = new Board(3, 3);
+      board.setState('***' + '*.*' + '***');
+      const numAlive = board.findLivingNeighbours(5);
+      expect(numAlive).toEqual(8);
+    });
+  });
 
-            console.log(board.render());
+  describe('Find coords on board', () => {
+    it('should find coords for index', () => {
+      const board = new Board(2, 4);
+      expect(board.findCoordsFor(1)).toEqual({ row: 1, col: 1 });
+      expect(board.findCoordsFor(2)).toEqual({ row: 1, col: 2 });
+      expect(board.findCoordsFor(3)).toEqual({ row: 1, col: 3 });
+      expect(board.findCoordsFor(4)).toEqual({ row: 1, col: 4 });
+      expect(board.findCoordsFor(5)).toEqual({ row: 2, col: 1 });
+      expect(board.findCoordsFor(6)).toEqual({ row: 2, col: 2 });
+      expect(board.findCoordsFor(7)).toEqual({ row: 2, col: 3 });
+      expect(board.findCoordsFor(8)).toEqual({ row: 2, col: 4 });
+    });
+  });
 
-                
-            board.update();
+  describe('Run game of life', () => {
+    it('should update simple 3x3 board', () => {
+      const board = new Board(3, 3);
+      board.setState('**.' + '*..' + '...');
 
-            console.log(board.render());
-        })
+      console.log(board.render());
 
-        it('should run one round', () => {
+      board.update();
 
-            const state = '............*......**...........';
-            const board = new Board(4,8);
-            board.setState(state);
+      console.log(board.render());
+    });
 
+    it('should run one round', () => {
+      const state = '............*......**...........';
+      const board = new Board(4, 8);
+      board.setState(state);
 
-            console.log(board.render());
-            
-            expect(board.findLivingNeighbours(12)).toBe(3);
-            board.update();
+      console.log(board.render());
 
-            const out = board.render();
+      expect(board.findLivingNeighbours(12)).toBe(3);
+      board.update();
 
-            console.log(board.render());
-            
-            expect(out).toEqual(
-                '........\n'+
-                '...**...\n'+
-                '...**...\n'+
-                '........\n');
-        })
-    })
+      const out = board.render();
 
+      console.log(board.render());
+
+      expect(out).toEqual(
+        '........\n' + '...**...\n' + '...**...\n' + '........\n',
+      );
+    });
+  });
 });
